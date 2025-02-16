23073caef278e35d5aab3db2a1d0607c4f65de7c
LeeYongSeok13 <minamiharuka333@gmail.com>
2025-02-16 12:56:10 +0900

    HashRouter 사용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage.jsx";
 import Project from "./pages/Project.jsx";
 import Profile from "./pages/Profile.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/project" element={<Project />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
